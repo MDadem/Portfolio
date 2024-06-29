43dc50e9d784f696b4def176d154323511d16f68
MDadem <miladiadem58@gmail.com>
2024-06-29 16:51:49 +0100

    Adding some home page feature

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,47 @@
+import Social from "@/components/Social";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
 export default function Home() {
   return (
-  <main>Home page</main>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col  xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            {/* text */}
+            <span className="text-xl">Web Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br /> <span className="text-accent">Adem MILADI</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              Passionate web developer skilled in crafting user-centric
+              experiences. Builds beautiful, responsive websites with modern
+              technologies.
+            </p>
+            {/* cv and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center 
+                text-accent text-base hover:bg-accent 
+                hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          {/* photo */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
   );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
         {/*logo*/}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
-            Adem MILADI <span className="text-accent">.</span>
+            Welcome<span className="text-accent">.</span>
           </h1>
         </Link>
         {/* desktop nav & hire me button*/}
