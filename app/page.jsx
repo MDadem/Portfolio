228d20412f8f6fa5dd9d41e7f656969a91663438
Adem <91350744+MDadem@users.noreply.github.com>
2024-06-30 02:08:47 +0100

    Adding photo & stats to home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,6 @@
+import Photo from "@/components/Photo";
 import Social from "@/components/Social";
+import Stats from "@/components/Stats";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
@@ -7,8 +9,8 @@ export default function Home() {
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col  xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
-            {/* text */}
+           {/* text */}
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Web Developer</span>
             <h1 className="h1 mb-6">
               Hello I'm <br /> <span className="text-accent">Adem MILADI</span>
@@ -39,9 +41,12 @@ export default function Home() {
             </div>
           </div>
           {/* photo */}
-          <div>photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 }
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,8 +1,40 @@
+"use client";
 
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum optio quidem voluptates maxime praesentium aperiam ex similique temporibus impedit, ad distinctio aut natus sit unde aliquid nemo doloremque dolorum sunt.",
+    href: "",
+  },
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum optio quidem voluptates maxime praesentium aperiam ex similique temporibus impedit, ad distinctio aut natus sit unde aliquid nemo doloremque dolorum sunt.",
+    href: "",
+  },
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum optio quidem voluptates maxime praesentium aperiam ex similique temporibus impedit, ad distinctio aut natus sit unde aliquid nemo doloremque dolorum sunt.",
+    href: "",
+  },
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum optio quidem voluptates maxime praesentium aperiam ex similique temporibus impedit, ad distinctio aut natus sit unde aliquid nemo doloremque dolorum sunt.",
+    href: "",
+  },
+];
 const Services = () => {
-  return (
-    <div>Services page</div>
-  )
-}
+  return <div>Services page</div>;
+};
 
-export default Services
+export default Services;
